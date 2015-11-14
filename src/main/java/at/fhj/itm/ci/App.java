0488d41193d8d4095a549dd600440bc904c2631a
Michael Stifter <michael.stifter2@edu.fh-joanneum.at>
2015-11-14 21:04:08 +0100

    Added demo project with tests

---
+++ src/main/java/at/fhj/itm/ci/App.java
@@ -0,0 +1,15 @@
+package at.fhj.itm.ci;
+
+public class App {
+	
+	private String hello;
+
+	public App() {
+		this.hello = "Hi ITM13";
+	}
+	
+	public String getHello() {
+		return this.hello;
+	}
+	
+}
---
+++ src/test/java/at/fhj/itm/ci/test/CITest.java
@@ -0,0 +1,24 @@
+package at.fhj.itm.ci.test;
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import at.fhj.itm.ci.App;
+
+
+public class CITest {
+	
+	App myApp;
+	
+	@Before
+	public void setup() {
+		myApp = new App();
+	}
+
+	@Test
+	public void test() {
+		assertEquals("Hi ITM13", myApp.getHello());
+	}
+
+}
